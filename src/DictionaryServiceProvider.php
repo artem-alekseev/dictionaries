28b155bc871029486d9996c9a74f3683e061ce97
Artem Alekseev <lexx73652@gmail.com>
2021-11-26 14:48:00 +0400

    Init commit

---
+++ src/DictionaryServiceProvider.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Dictionaries;
+
+use App\Console\Commands\CreateDictionaryCommand;
+use Illuminate\Support\ServiceProvider;
+
+class DictionaryServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any package services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        if ($this->app->runningInConsole()) {
+            $this->commands([
+                CreateDictionaryCommand::class
+            ]);
+        }
+
+        $this->publishes([
+            __DIR__.'/Dictionary.php' => app_path('Models/Dictionary.php')
+        ]);
+    }
+}
